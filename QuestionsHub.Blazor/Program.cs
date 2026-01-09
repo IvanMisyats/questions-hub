5f349a5ed2629f397e5c0d69b26fc7324ce11952
Ivan Misyats <ivan@misyats.com>
2026-01-09 19:29:25 +0100

    Special characters are no longer required in a password

--- QuestionsHub.Blazor/Program.cs
+++ QuestionsHub.Blazor/Program.cs
@@ -47,7 +47,7 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
         options.Password.RequireDigit = true;
         options.Password.RequireLowercase = true;
         options.Password.RequireUppercase = true;
-        options.Password.RequireNonAlphanumeric = true;
+        options.Password.RequireNonAlphanumeric = false;
         options.Password.RequiredLength = 8;
         options.Password.RequiredUniqueChars = 1;
 
